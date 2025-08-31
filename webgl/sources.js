d5ae8bf5ae87e6cec4738a07ce24359883705dc9
Maxime Bolleau <maximebolleau@mac.home>
2025-08-31 12:14:26 +0200

    import dist initial

---
+++ webgl/sources.js
@@ -0,0 +1,21 @@
+export default [
+  /*
+  {
+    name: "myCustomName",
+    type: "gltfModel" || "fbxModel" || "cubeTexture" || "texture",
+    path: "webgl/{folder}/{name}/{file}" || []
+  }
+  */
+
+  {
+    name: 'box',
+    type: 'gltfModel',
+    path: 'webgl/models/box.glb'
+  },
+
+  {
+    name: 'perso1',
+    type: 'texture',
+    path: 'webgl/textures/perso1.png'
+  }
+]
